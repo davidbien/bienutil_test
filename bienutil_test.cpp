ddf62850a4ecd694fc29a601bd503b0a808e4bd5
David Bien <davidbien@hotmail.com>
2021-03-08 12:12:24 -0700

    Add project files.

---
+++ bienutil_test.cpp
@@ -0,0 +1,12 @@
+﻿// bienutil_test.cpp : Defines the entry point for the application.
+//
+
+#include "bienutil_test.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
